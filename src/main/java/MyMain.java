6505ef281351466000a9cbd50c2f5337891d64f0
2022jobyrne <22jobyrne@athenian.org>
2020-09-29 05:12:43 +0000

    not complete - i spent more than an hour and a half on it

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,20 +5,48 @@ public class MyMain {
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        int val = (int) ((Math.random() * 6) + 1);
+        return val;
     }
 
     // Rolls two dice n times
     // This method returns an int array of size 11, where each element is
     // the number of times a certain sum of two dice was rolled
-    public static int[] sumOfTwoDice(int n) {
-        // REPLACE YOUR CODE HERE
+    public static String sumOfTwoDice(int n) {
+        int die1 = 0;
+        int die2 = 0;
+        int[] nums = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int sum = 0;
+        int inte = 0;
+        int integer = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            die1 = (int) ((Math.random() * 6) - 1);
+            die2 = (int) ((Math.random() * 6) - 1);
+            sum = (die1 + die2);
+            //integer = inte + 1;
+            nums[sum] = 1;
+//nums = 1, 2, 3, 4, 5, 6...
+        }
+         for (int j = 0; j <= nums.length; j++) {
+            String q = "sum were";      
+            return (n + q + nums[j]);
+            }
+        //int[] arr = {1, 2, 3};
+        //return nums;
         return null;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many times?");
+        int vals = scan.nextInt();
+        System.out.println(rollDie());
+        for (int i = 0; i <= vals; i++) {
+            System.out.println(sumOfTwoDice(vals));
+            System.out.println(sumOfTwoDice(vals));
+        }
+        
+        }
     }
-}
+
